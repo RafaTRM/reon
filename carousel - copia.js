50b65a9144116ba7026de7be376ce68c14835b50
RafaTRM <68725627+RafaTRM@users.noreply.github.com>
2020-09-05 19:03:19 -0500

    Add files via upload

---
+++ carousel - copia.js
@@ -0,0 +1,136 @@
+//SELECT CARRUSEL
+const carousel = document.querySelector(".carousel");
+
+
+//select next button
+const nextButton = document.querySelector(".right-btn");
+
+//select left button
+const previousButton = document.querySelector(".left-btn"   );
+
+//select the navcarousel
+const nav = document.querySelector(".nav");
+
+
+//select all the dots
+const dots = [...nav.children];
+
+
+//select all the slides inside the carousel
+const slides = [...carousel.children];
+
+
+//calculate the slide whith
+let slideWidth = slides[0].getBoundingClientRect().width;
+
+//position the slides horizontaly
+function positionSlides(slides) {
+    for (let index = 0; index < slides.length; index++){
+        slides[index].style.left = slideWidth * index + "px";
+    }
+}
+
+positionSlides(slides);
+
+
+//On right button click, we move (translateX) the carousel to the left
+nextButton.addEventListener("click", function(){
+    const currentSlide = carousel.querySelector(".active");
+    const nextSlide = currentSlide.nextElementSibling;
+
+    moveToSlide(carousel, currentSlide, nextSlide);
+    hideButton(nextSlide, slides);
+
+    moveToDot(nextSlide, slides, nav, dots);
+    
+});
+
+
+//On left    button click, we move (translateX) the carousel to the right
+previousButton.addEventListener("click", function(){
+    const currentSlide = carousel.querySelector(".active");
+    const previousSlide = currentSlide.previousElementSibling;
+
+    moveToSlide(carousel, currentSlide, targetSlide);
+    hideButton(previousSlide, slides);
+    moveToDot(previousSlide, slides, nav, dots);
+    
+});
+
+//on dot click
+nav.addEventListener("click", function(e){
+    //if we didnt click on a dot, we exit
+    if(e.target === nav) return;    
+
+    //select th clicked dot    
+    const targetDot = e.target;
+
+    //select the current dot
+    const currentDot = nav.querySelectorAll(".active");
+
+
+    //select the current slide
+    const currentSlide = carousel.querySelectorAll(".active");
+
+    //find the index of the dot, so we can target the right slide
+    let targetDotIndex = findIndex(targetDot, dots);
+
+
+    //select the target slide
+    const targetSlide = slides[targetDotIndex];
+    moveToSlide(carousel, currentSlide, targetSlide);   
+    toogleActive(currentDot, targetDot);
+    hideButton(targetSlide, slides);
+
+})
+
+//move to dot
+function moveToDot(targetSlide, slides, nav, dots){
+    let slideIndex = findIndex(targetSlide, slides);
+    const currentDot = nav.querySelector(".active");
+    const targetDot = dots[slideIndex];
+    toogleActive (currentDot, targetDot);
+} 
+
+
+
+
+//move to slide
+function moveToSlide(carousel, currentSlide, targetSlide){
+    const position = targetSlide.style.left;
+    carousel.style.transform = 'translateX(-${position})';
+    toogleActive(currentSlide, targetSlide);
+}
+
+//toogle active class
+function toogleActive(current, target){
+    current.classList.remove(".active");
+    target.classList.add(".active");
+}
+
+
+//hide button
+function hideButton(targetSlide, slides){
+    if(targetSlide === slides[0]){
+        previousButton.classList.add(".hide");
+        nextButton.classList.remove(".hide");
+    } else if (targetSlide === slides[slides.length - 1]){
+        nextButton.classList.add(".hide");
+        previousButton.classList.remove("hide");
+    } else {
+        previousButton.classList.add("hide");    
+        nextButton.classList.remove(".hide");
+        
+
+    } 
+}
+
+//find the index of an item inside an array of items
+function findIndex(item, items){
+    for(let index = 0; index < items.length; index++){
+        if(item === items[index]){
+            return index;
+        }
+    }
+
+}
